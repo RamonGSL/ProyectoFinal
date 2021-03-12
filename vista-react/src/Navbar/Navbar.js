dbe05af2cb377e1ce2738a1c05a0cb7fd0f54a09
RamonGSL <ramongarciadelaserrana@gmail.com>
2021-03-13 00:21:34 +0100

    navbar2.0 modoOscuro1rParte

--- vista-react/src/Navbar/Navbar.js
+++ vista-react/src/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+
+
 //imports de react-bootstrap
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -14,92 +16,124 @@ import AccountBoxRoundedIcon from "@material-ui/icons/AccountBoxRounded";
 import Dropdown from "react-bootstrap/Dropdown";
 import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "animate.css";
-//
-//import {Link} from 'react-router-dom';
-
 
 const NavbarComponent = () => {
-    let golden = '#BEB06B';
-    let white = '#F9FAF6';
+//var para comprobar en que modo esta
+let darkMode = false;
+
+//variables de las fuentes y backgrounds
+let fontWhite ="black";
+let fontDark ="#F9FAF6";
+let backgroundWhite ="#A19C9C";
+let backgroundDark ="#47464A";
+
+
+
+//constante para cambiar el color del link respecto al router
+const clickLink = (numberLink) => {
+  let icons = document.getElementsByClassName("icon");
+  for(let i = 0; i<=3; i++){
+    if(icons[i].classList.contains("selectLink")){
+      icons[i].classList.remove("selectLink");
+    }
+  }
+  if(numberLink === 2 || numberLink === 3){
+    document.getElementById("ZoneUser").classList.add("selectLink");
+   
+  }else{
+    document.getElementById("ZoneUser").classList.remove("selectLink");
+  }
+  icons[numberLink].classList.add("selectLink")
+}
+
+//constante para el darkMode
+const changeColorMode = () =>{
   
-    const click = (variable) =>{
+  if(darkMode === false){
+    darkMode = true;
+  }else{
+    darkMode = false;
+  }
+  if(darkMode === false){
+    //Aquí cambiamos las variables globales del color de fondo y fuente para toda la web
+    document.documentElement.style.setProperty('--fontColor',fontWhite);
+    document.documentElement.style.setProperty('--backgroundColor',backgroundWhite);
+    document.getElementById("colorMode").style.color = "black";
 
-      if(variable !== '--colorMode')
-      {
-        document.documentElement.style.setProperty('--colorHome',white);
-        document.documentElement.style.setProperty('--colorHotels',white);
-        document.documentElement.style.setProperty('--colorZoneUser',white);
-        document.documentElement.style.setProperty('--colorRegister',white);
-        document.documentElement.style.setProperty('--colorLogin',white);
-
-        document.documentElement.style.setProperty(variable ,golden);
-
-        if(variable === "--colorLogin" || variable === "--colorRegister"){
-          document.documentElement.style.setProperty('--colorZoneUser',golden);
-        }
-
-      }else{
-       let colorMode = getComputedStyle(document.documentElement).getPropertyValue(variable);
-       if(colorMode === white){
-        document.documentElement.style.setProperty(variable,golden);
-       }else{
-        document.documentElement.style.setProperty(variable,white);
-       }
-      }
-      
-       
+    //Aquí cambiamos los bordes del dropdown
+    document.documentElement.style.setProperty('--borderUserZones',fontWhite);
     
+    
+
+  }else{
+    //Repetimos para el modo oscuro
+    document.documentElement.style.setProperty('--fontColor',fontDark);
+    document.documentElement.style.setProperty('--backgroundColor',backgroundDark);
+    document.getElementById("colorMode").style.color = "#BEB06B";
+    
+    document.documentElement.style.setProperty('--borderUserZones','var(--colorGolden)');
+
     
   }
+}
 
 
-  return (
-    <Navbar collapseOnSelect className="Navbar" sticky="top" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  return ( 
+    <div id="containerNavbar" className="colorMode">
+    <Navbar collapseOnSelect className="Navbar" expand="lg">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Collapse id="basic-navbar-nav">
 
-        <Nav className="mr-auto Nav" as="ul">
-          <Nav.Link  className="NavLink1 classWhite" to="/" onClick={() => click('--colorHome')} as="li" >
-            <HomeRoundedIcon id="iconHome"></HomeRoundedIcon> Home
-          </Nav.Link>
-          <Nav.Link className="NavLink2 classWhite" to="/hotels"  onClick={() =>click('--colorHotels')} as="li">
-            <ApartmentRoundedIcon></ApartmentRoundedIcon> Hotels
-          </Nav.Link>
-        </Nav>
+      <Nav className="mr-auto">
+        <Nav.Link to="/" onClick={() => clickLink(0)}>
+          <div className="fontColor icon">
+          <HomeRoundedIcon></HomeRoundedIcon> Home
+          </div>
+        </Nav.Link>
+        <Nav.Link to="/hotels" onClick={() => clickLink(1)}>
+          <div  className="fontColor icon">
+          <ApartmentRoundedIcon></ApartmentRoundedIcon> Hotels
+          </div>
+        </Nav.Link>
+      </Nav>
 
-        <Nav className="mr-auto Nav1 animate__animated animate__pulse" >
-          <Nav.Item>Holy Rest</Nav.Item>
-        </Nav>
+      <Nav className="mr-auto" >
+        <Nav.Item>
+          <div id="title" className="fontColor">
+            Holy Rest
+            </div>
+          </Nav.Item>
+      </Nav>
 
-        <Nav className="Nav2">
-          <Button className="NavButton classWhite" onClick={() =>click('--colorMode')}>
-            <Brightness4RoundedIcon></Brightness4RoundedIcon> DARK MODE
-          </Button>
+      <Nav>
+        <Button id="colorMode" className="NavButton" onClick={() =>changeColorMode()}>
+          <Brightness4RoundedIcon></Brightness4RoundedIcon> DARK MODE
+        </Button>
 
-          <Dropdown className="Dropdown">
-            <Dropdown.Toggle className="Dropdown_Toogle">
-              <AccountCircleRoundedIcon></AccountCircleRoundedIcon>
-              Zone User
-             
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu className="Dropdown_Menu">
+        <Dropdown>
+          <Dropdown.Toggle id="ZoneUser" className="Dropdown_Toogle colorMode fontColor">
+            <AccountCircleRoundedIcon></AccountCircleRoundedIcon>
+            Zone User
+           
+          </Dropdown.Toggle>
+          
+          <Dropdown.Menu classID="drop" className="Dropdown_Menu colorMode UserZoneBorder">
               <div>
-                <Dropdown.Item className="NavDropdownA classWhite" href="#Register"  onClick={() => click('--colorRegister')}>
-                  <VpnKeyRoundedIcon></VpnKeyRoundedIcon> Register
-                </Dropdown.Item>
-                <Dropdown.Item className="NavDropdownB classWhite" href="#Login"  onClick={() => click('--colorLogin')}>
-                  <AccountBoxRoundedIcon></AccountBoxRoundedIcon> Login
-                </Dropdown.Item>
+              <Dropdown.Item className="fontColor colorMode icon" to="/" onClick={() => clickLink(2)}>
+                <VpnKeyRoundedIcon></VpnKeyRoundedIcon> Register
+              </Dropdown.Item>
+              <Dropdown.Item className="fontColor colorMode icon" to="/" onClick={() => clickLink(3)}>
+                <AccountBoxRoundedIcon></AccountBoxRoundedIcon> Login
+              </Dropdown.Item>
               </div>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
-
+          </Dropdown.Menu>
+        </Dropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+  </div>
+   );
+}
+ 
 export default NavbarComponent;
